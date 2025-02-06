f3c0c32069069420a0ea3792409e46af2309be8b
CorwinAnsley <coreyansley@gmail.com>
2025-02-06 15:25:37 +0000

    finished practical 1

--- id_gen_vars/practical_1.R
+++ id_gen_vars/practical_1.R
@@ -4,4 +4,42 @@ class(anova_data$housing)
 
 summary(anova_data)
 
-levelsbree
+levels(anova_data$breed)
+
+quantile(anova_data$weight, prob=c(0.025,0.975))
+
+t.test(anova_data$weight ~ anova_data$housing)
+
+wilcox.test(anova_data$weight ~ anova_data$housing)
+
+chisq.test(table(anova_data$housing,anova_data$breed))
+
+## 1a
+hist(anova_data$weight)
+
+shapiro.test(anova_data$weight)
+
+## 1b
+plot(anova_data$tick, anova_data$weight, pch=15, col='red', cex.lab=1.5)
+
+lm_1 = lm(weight~tick, data=anova_data)
+summary(lm_1)
+par(mfrow=c(2,2))
+abline(lm(weight~tick, data=anova_data),col="blue",lwd=3) 
+
+plot(lm_1)
+
+par(mfrow=c(1,1))
+plot(anova_data$tick, anova_data$weight, pch=15, col='red', cex.lab=1.5,
+     abline(lm(weight~tick, data=anova_data),col="blue",lwd=3) )
+
+## 2
+
+boxplot(anova_data$weight~anova_data$breed, xlab="Breed", ylab="Weight",col="red")
+
+aov1 = aov(weight~breed, data = anova_data)
+summary(aov1)
+
+pairwise.t.test(anova_data$weight, anova_data$breed, p.adj = "bonf")
+
+pairwise.t.test(anova_data$weight, anova_data$breed, p.adj = "fdr")
